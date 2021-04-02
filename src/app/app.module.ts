945dc7b173ceeaf27b6563b48b9fb20e1779ccf3
Your Name <vladedumail@gmail.com>
2021-04-02 13:06:56 +0300

    test commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { FormComponent } from './form/form.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
